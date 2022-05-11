5b99002862da2228006ffbfb7de1eda0972a7c5e
EmilFreijd <emil.freijd@gmail.com>
2022-05-11 08:21:37 +0200

    First daily completed!

---
+++ Python/001-char-input.py
@@ -0,0 +1,7 @@
+print("Hello & welcome to the time machine!")
+
+name = input("Whats your name? ")
+age = int(input("How old are you? "))
+years = int(input("How many years do you want to travel? "))
+
+print("SWOOOSH, welcome to the future " + name + ", your current age is " + str(age + years) + " that is really old.")
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # milionaire-morning
+
